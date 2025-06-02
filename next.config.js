e8a057be9c328f9218b0a470f15608b8134af09c
231Brooks <anthony.brooks1201@gmail.com>
2025-06-02 16:29:10 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify is no longer a valid option in Next.js 15+ and will be ignored.
-  // Remove or comment out the line below to avoid warnings.
-  // swcMinify: true, 
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -13,9 +10,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // If you are using the /app directory for routing, you must enable this:
   experimental: {
     appDir: true,
+    serverActions: true,
   },
 }
 
